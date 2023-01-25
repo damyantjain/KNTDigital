5c5a2201819c57c90b2523f7946292dacd1483d5
Damyant Jain <Damyant9999@gmail.com>
2023-01-26 00:57:21 +0530

    Updated Bootstrap and edit employee

--- kntdigital/action/employee/forms.py
+++ kntdigital/action/employee/forms.py
@@ -2,9 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, Optional, Regexp, ValidationError
 from kntdigital.models.user import User
+from flask_login import current_user
 
 
-class AddEmployeeForm(FlaskForm):
+class AddorEditEmployeeForm(FlaskForm):
     first_name = StringField("First Name*", validators=[DataRequired()])
     last_name = StringField("Last Name*", validators=[DataRequired()])
     email = StringField("Email*", validators=[DataRequired(), Email()])
@@ -24,9 +25,11 @@ class AddEmployeeForm(FlaskForm):
         ("Other"),
     ]
     gender = SelectField("Gender", choices=choices, default="Male")
-    submit = SubmitField("Add Employee")
+    submit = SubmitField("Save")
 
     def validate_email(self, email):
+        if email.data == current_user.email:
+            return
         user = User.query.filter_by(email=email.data.lower()).first()
         if user:
-            raise ValidationError("That email is taken. Please choose a different one")
+            raise ValidationError(f"That email is taken. Please choose a different one")
